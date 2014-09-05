65477784918b38b8563caffbfe936269973e9c15
fakugelp <faku_gelp@hotmail.com>
2014-09-05 02:48:53 -0300

    Update AcmeDemoBundle.php

--- src/Acme/DemoBundle/AcmeDemoBundle.php
+++ src/Acme/DemoBundle/AcmeDemoBundle.php
@@ -7,4 +7,6 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 class AcmeDemoBundle extends Bundle
 {
     //**prueba comentario acmedemobundle**//
+    
+    //**prueba edicion de archivo desde github**//
 }
