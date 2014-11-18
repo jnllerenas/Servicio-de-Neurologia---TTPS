5d31359bddf03b93e15cd15b13f831db2cc7e415
jsivori <jonatansivori@gmail.com>
2014-11-18 18:27:48 -0300

    borrado de cosas al pedo

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
-            //new Neurologia\FirstBundle\NeurologiaFirstBundle(),
             new Neurologia\GenericosBundle\NeurologiaGenericosBundle(),
             new Neurologia\UserBundle\NeurologiaUserBundle(),
 			new Neurologia\DiagnosticoBundle\NeurologiaDiagnosticoBundle(),
@@ -29,7 +28,6 @@ class AppKernel extends Kernel
 
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            //$bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
