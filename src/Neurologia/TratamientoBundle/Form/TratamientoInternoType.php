3e14cb139f8e56c0fc57084416d3da0fc1a74646
scaporale <seba_snm_90@hotmail.com>
2014-11-24 15:15:45 -0300

    Sigo arreglando guardado de tratamientos internos, relación ternaria complicada en su posteo...

--- src/Neurologia/TratamientoBundle/Form/TratamientoInternoType.php
+++ src/Neurologia/TratamientoBundle/Form/TratamientoInternoType.php
@@ -5,6 +5,8 @@ namespace Neurologia\TratamientoBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Neurologia\GenericosBundle\Form\DrogaType;
+//use Neurologia\GenericosBundle\Form\EfectoAdversoType;
 
 class TratamientoInternoType extends AbstractType
 {
@@ -16,10 +18,15 @@ class TratamientoInternoType extends AbstractType
     {
         $builder
             ->add('descripcion', 'textarea', array('required'  => true, 'label' => 'Descripción'))
-            ->add('inicio', 'date', array('required'  => true, 'label' => 'Fecha de inicio'))
+            ->add('inicio', 'date', array('required'  => true, 
+                                          'label' => 'Fecha de inicio'))
             ->add('activo', 'choice', array('choices'   => array('1' => 'Si', '0' => 'No'),
                                             'required'  => true,
                                             'label' => 'Está activo?'))
+//            ->add('drogas', 'collection', array('label' => 'blebleblelebelebe',
+//                                                'type' => new DrogaType(),
+//                                                'allow_add' => true,
+//                                                'allow_delete' => true))
             ->add('aceptar', 'submit', array('label' => 'Aceptar'))
         ;
     }
