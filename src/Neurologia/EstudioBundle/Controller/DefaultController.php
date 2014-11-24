230e20bb1a2759ed4cbfb42388b2f79a2e9a0125
Facundo Basso <faku_gelp@hotmail.com>
2014-11-24 16:33:15 -0300

    validaciones

--- src/Neurologia/EstudioBundle/Controller/DefaultController.php
+++ src/Neurologia/EstudioBundle/Controller/DefaultController.php
@@ -1,47 +1,54 @@
 <?php
 
 namespace Neurologia\EstudioBundle\Controller;
+
 use Neurologia\BDBundle\Entity\Estudio;
 use Neurologia\EstudioBundle\Form\Type\EstudioType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+class DefaultController extends Controller {
 
-class DefaultController extends Controller
-{
-        
-
-        public function nuevoAction(Request $request)
-    {
+    public function nuevoAction(Request $request) {
         $estudio = new Estudio();
+        $error = "";
 
         $form = $this->createForm(new EstudioType(), $estudio);
 
+        $cloned = clone $form;
+
         $form->handleRequest($request);
-        
-               
-   
+
+
+
+
         if ($form->isValid()) {
             // ... maybe do some form processing, like saving the Task and Tag objects
-            
-        $em = $this->getDoctrine()->getManager();        
-        $em->persist($estudio);
-        $em->flush();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($estudio);
+            $em->flush();
+            return $this->redirect($this->generateUrl('neurologia_estudio_success'));
+        }
+
         
-            return $this->redirect($this->generateUrl('neurologia_estudio_nuevo'));
+        if (!($form->get('imagenes')->isEmpty())) {
+            $error = $form->get('imagenes')->getErrorsAsString();
+            $form = $cloned;            
         }
         
-// var_dump($form->getErrors());
-        
-        
-       
+
+
+
         return $this->render('NeurologiaEstudioBundle:Default:nuevo.html.twig', array(
-            'form' => $form->createView(),
+                    'form' => $form->createView(), 'error' => $error
         ));
     }
     
+        public function successAction() {
+
+
+        return $this->render('NeurologiaEstudioBundle:Default:success.html.twig');
+    }
 
 }
-
-    
-
