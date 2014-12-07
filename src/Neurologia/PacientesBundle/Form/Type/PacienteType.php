620db60de295848f5c4a70952b38cacce09d7ef8
jsivori <jonatansivori@gmail.com>
2014-12-07 14:47:19 -0300

    cambios en el alta de paciente

--- src/Neurologia/PacientesBundle/Form/Type/PacienteType.php
+++ src/Neurologia/PacientesBundle/Form/Type/PacienteType.php
@@ -9,25 +9,29 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 class PacienteType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
-
-        $builder->add('nombre', 'text');
-        $builder->add('apellido', 'text');
-        $builder->add('direccion', 'text');
-        $builder->add('documento', 'text');
+		
+        $builder->add('nombre', 'text', array('attr'=>array('size'=>25)));
+        $builder->add('apellido', 'text', array('attr'=>array('size'=>25)));
+		$builder->add('sexo');
+		$builder->add('tipoDocumento');
+		$builder->add('documento', 'text', array('attr'=>array('size'=>10)));
         $builder->add('fechaNacimiento', 'date', array(
             'widget' => 'single_text'));
-        $builder->add('telefono', 'text');
-        $builder->add('ocupacion', 'text');
-        $builder->add('otros', 'textarea');
-        $builder->add('numeroCarnet', 'text');
-        $builder->add('email', 'email');
-        $builder->add('nivelEducacional');
-        $builder->add('obraSocial');
+        $builder->add('telefono', 'text', array('attr'=>array('size'=>12)));
+        
+		$builder->add('direccion', 'text', array('required' => false, 'attr'=>array('size'=>15))); 
+		$builder->add('email', 'email', array('required' => false, 'attr'=>array('size'=>15)));
+        $builder->add('ocupacion', 'text', array('required' => false, 'attr'=>array('size'=>20)));
+        $builder->add('otros', 'textarea', array('required' => false, 'attr'=>array('style'=>'height:34px')));
+        $builder->add('numeroCarnet', 'text', array('attr'=>array('size'=>12)));
+       
         $builder->add('estadoCivil');
-        $builder->add('tipoDocumento');
+		$builder->add('nivelEducacional');
+        $builder->add('obraSocial');
+        
         $builder->add('admitidoPor');
         $builder->add('derivadoPor');
-        $builder->add('sexo');
+        
         //$builder->add('save', 'submit', array('label' => 'Alta paciente'));
     }
 
