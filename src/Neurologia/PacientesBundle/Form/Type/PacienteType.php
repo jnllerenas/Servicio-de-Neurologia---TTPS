f2577ff69247bd08aa7b1d7c9a795b2733b74be6
Jonatan Nahuel Llerena Suster <jnllerenas@gmail.com>
2014-12-09 15:07:00 -0300

    estilo form paciente

--- src/Neurologia/PacientesBundle/Form/Type/PacienteType.php
+++ src/Neurologia/PacientesBundle/Form/Type/PacienteType.php
@@ -10,24 +10,23 @@ class PacienteType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
 		
-        $builder->add('nombre', 'text', array('attr'=>array('size'=>25)));
-        $builder->add('apellido', 'text', array('attr'=>array('size'=>25)));
-		$builder->add('sexo');
-		$builder->add('tipoDocumento');
-		$builder->add('documento', 'text', array('attr'=>array('size'=>10)));
+        $builder->add('nombre', 'text', array('attr'=>array('size'=>25,'class' => 'form-control')));
+        $builder->add('apellido', 'text', array('attr'=>array('size'=>25,'class' => 'form-control')));
+        $builder->add('sexo',null, array( 'attr' => array( 'class' => 'form-control')));
+        $builder->add('tipoDocumento',null, array( 'attr' => array( 'class' => 'form-control')));
+        $builder->add('documento', 'text', array('attr'=>array('size'=>10,'class' => 'form-control')));
         $builder->add('fechaNacimiento', 'date', array(
             'widget' => 'single_text'));
-        $builder->add('telefono', 'text', array('attr'=>array('size'=>12)));
-        
-		$builder->add('direccion', 'text', array('required' => false, 'attr'=>array('size'=>15))); 
-		$builder->add('email', 'email', array('required' => false, 'attr'=>array('size'=>15)));
-        $builder->add('ocupacion', 'text', array('required' => false, 'attr'=>array('size'=>20)));
-        $builder->add('otros', 'textarea', array('required' => false, 'attr'=>array('style'=>'height:34px')));
-        $builder->add('numeroCarnet', 'text', array('attr'=>array('size'=>12)));
+        $builder->add('telefono', 'text', array('attr'=>array('size'=>12,'class' => 'form-control')));
+        $builder->add('direccion', 'text', array('required' => false, 'attr'=>array('size'=>25,'class' => 'form-control'))); 
+        $builder->add('email', 'email', array('required' => false, 'attr'=>array('size'=>25,'class' => 'form-control')));
+        $builder->add('ocupacion', 'text', array('required' => false, 'attr'=>array('size'=>20,'class' => 'form-control')));
+        $builder->add('otros', 'textarea', array('required' => false, 'attr'=>array('style'=>'height:34px','class' => 'form-control')));
+        $builder->add('numeroCarnet', 'text', array('attr'=>array('size'=>12,'class' => 'form-control')));
        
-        $builder->add('estadoCivil');
-		$builder->add('nivelEducacional');
-        $builder->add('obraSocial');
+        $builder->add('estadoCivil',null, array( 'attr' => array( 'class' => 'form-control')));
+        $builder->add('nivelEducacional', null, array( 'attr' => array( 'class' => 'form-control')));
+        $builder->add('obraSocial', null, array( 'attr' => array( 'class' => 'form-control')));
         
         /*$builder->add('admitidoPor');
         $builder->add('derivadoPor');*/
